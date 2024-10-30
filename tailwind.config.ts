1af1369cf867570a66a80fb4dae44c8a73da6bb2
kun.liu <kun.liu@outlook.com>
2024-10-30 12:54:30 +0800

    fixed bug which eslint doesnot allow require in `tailwind.config.ts`.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssanimate from "tailwindcss-animate";
 
 const config: Config = {
     darkMode: ["class"],
@@ -58,6 +59,6 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssanimate],
 };
 export default config;
